8ec9c9594f5adeea42a5b131c480d11e6ba0351f
Logan Coates <coates.logan1125@gmail.com>
2021-05-10 14:39:09 -0600

    changed cards

--- portfolio/src/components/Projectcard.js
+++ portfolio/src/components/Projectcard.js
@@ -1,69 +1,37 @@
 import React from "react";
 import "./Projectcard.css";
 import "../App.css";
-import Clicky from "../images/click-game.png";
-import Budget from "../images/budget.png";
-import Note from "../images/note-taker.png";
+import CardItem from "../components/CardItem";
 
 function Projectcard() {
   return (
     <div>
-      <div className="card-group">
-        <div className="card1">
-          <img src={Clicky} className="card-img-top1" alt="bad" />
-          <div className="card-body">
-            <h5 className="card-title">Clicky Game</h5>
-            <p className="card-text">
-              This is a matching game where you click on an image and then as
-              they get shuffled you try not to pick the same image again.
-            </p>
-            <button type="button" className="btn btn-outline-dark">
-              <a
-                href="https://memory-games-project.herokuapp.com/"
-                className="card-link1"
-              >
-                Website link
-              </a>
-            </button>
-          </div>
-        </div>
-        <div className="card2">
-          <img src={Budget} className="card-img-top6" alt="budget" />
-          <div className="card-body">
-            <h5 className="card-title">Budget Tracker</h5>
-            <p className="card-text">
-              This is a app where you can track your money. You can add or
-              subtract the money and it shows up on a graph.
-            </p>
-            <button type="button" className="btn btn-outline-dark">
-              <a
-                href="https://secret-river-70452.herokuapp.com/"
-                className="card-link2"
-              >
-                Website link
-              </a>
-            </button>
-          </div>
-        </div>
-      </div>
-      <div className="card-group1">
-        <div className="card3">
-          <img src={Note} className="card-img-top9" alt="..." />
-          <div className="card-body">
-            <h5 className="card-title">Note Taker</h5>
-            <p className="card-text">
-              This is a wider card with supporting text below as a natural
-              lead-in to additional content. This card has even longer content
-              than the first to show that equal height action.
-            </p>
-            <button type="button" className="btn btn-outline-dark">
-              <a
-                href="https://shrouded-shelf-54377.herokuapp.com/"
-                className="card-link2"
-              >
-                Website link
-              </a>
-            </button>
+      <div className="cards">
+        <h1 className="title">My Projects</h1>
+        <div className="cards__container">
+          <div className="cards__wrapper">
+            <ul className="cards__items">
+              <CardItem
+                src="images/click-game.png"
+                text="This is a game where you have to remember the card you clicked on and cant click on that card again "
+                label="Memory Game"
+                path="/project"
+              />
+            </ul>
+            <ul className="cards__items">
+              <CardItem
+                src="images/budget.png"
+                text="This app can track you income and spending habits"
+                label="Budget"
+                path="https://memory-games-project.herokuapp.com/"
+              />
+              <CardItem
+                src="images/note-taker.png"
+                text="This app will help you take and keep track of notes"
+                label="Note taker"
+                path="/contact"
+              />
+            </ul>
           </div>
         </div>
       </div>
--- portfolio/src/pages/Contact.js
+++ portfolio/src/pages/Contact.js
@@ -2,46 +2,38 @@ import React from "react";
 import "../App.css";
 import "../pages/Contact.css";
 import Footer from "../components/Footer";
-import Email from "../images/email.png";
-import Linked from "../images/Linkedin.png";
-import github from "../images/github.jpg";
+import CardItem from "../components/CardItem";
 
 function Contact() {
   return (
     <div>
-      <div className="card-group">
-        <div className="card1">
-          <img src={Email} className="card-img-top" alt="Email Logo" />
-          <div className="card-body">
-            <h5 className="card-title">Email</h5>
-            <p className="card-text">Coates.logan1125@gmail.com</p>
+      <div className="cards">
+        <h1 className="title">Contact Me</h1>
+        <div className="cards__container">
+          <div className="cards__wrapper">
+            <ul className="cards__items">
+              <CardItem
+                src="images/github.jpg"
+                text="Check out all my works on my github page"
+                label="Github"
+                path="https://memory-games-project.herokuapp.com/"
+              />
+              <CardItem
+                src="images/email.png"
+                text="Coates.logan1125@gmail.com"
+                label="Email"
+                path="/contact"
+              />
+            </ul>
+            <ul className="cards__items">
+              <CardItem
+                src="images/Linked.png"
+                text="My Works and Projects "
+                label="LinkedIn"
+                path="/project"
+              />
+            </ul>
           </div>
-        </div>
-        <div className="card2">
-          <img src={Linked} className="card-img-top1" alt="..." />
-          <div className="card-body">
-            <h5 className="card-title">LinkedIn</h5>
-            <p className="card-text">
-            </p>
-            <button type="button" className="btn btn-outline-dark">
-              <a
-                href="https://www.linkedin.com/in/logan-coates-b404b61bb/"
-                className="card-link2"
-              >
-                Website link
-              </a>
-            </button>
-          </div>
-        </div>
-      </div>
-      <div className="card-group1">
-        <div className="card3">
-            <img src={github} className="card-img-top2" alt="..." />
-            <div className="card-body">
-            <h5 className="card-title">Github</h5>
-            <p className="card-text">Check out all my Github</p>
-            <button type="button" className="btn btn-outline-dark"><a href="https://github.com/coatsey" className="card-link2">Website link</a></button>
-            </div>
         </div>
       </div>
       <Footer />
